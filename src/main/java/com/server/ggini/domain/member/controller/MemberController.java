8fb148cd1cd0411f23a4dd640a184374c8f4a22d
박세준 <74056843+sejoon00@users.noreply.github.com>
2025-01-23 03:45:01 +0900

    [feat/#10] swagger jwt 달기

--- src/main/java/com/server/ggini/domain/member/controller/MemberController.java
+++ src/main/java/com/server/ggini/domain/member/controller/MemberController.java
@@ -3,9 +3,12 @@ package com.server.ggini.domain.member.controller;
 import com.server.ggini.domain.member.domain.Member;
 import com.server.ggini.domain.member.dto.response.MemberGetResponse;
 import com.server.ggini.global.annotation.AuthUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     @GetMapping("me")
+    @Operation(summary = "내 정보 조회", description = "jwt accessToken을 통해 내 정보를 조회합니다.")
     public ResponseEntity<MemberGetResponse> getMyInfo(
             @AuthUser Member member
             ) {
--- src/main/java/com/server/ggini/global/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/server/ggini/global/security/filter/JwtAuthenticationFilter.java
@@ -24,7 +24,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-
+        String authorization = request.getHeader("Authorization");
+        System.out.println("Authorization: " + authorization);
         String accessToken = extractAccessTokenFromHeader(request);
 
         if (StringUtils.hasText(accessToken)) {
