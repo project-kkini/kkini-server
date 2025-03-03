982a819dd64dfaec3e79735fe4a43e5fbbd99577
이준영 <uhiccup@gmail.com>
2025-03-03 22:31:14 +0900

    feat: tag 추가

--- src/main/java/com/server/ggini/domain/locationAuth/controller/LocationAuthController.java
+++ src/main/java/com/server/ggini/domain/locationAuth/controller/LocationAuthController.java
@@ -6,6 +6,7 @@ import com.server.ggini.domain.locationAuth.dto.request.LocationAuthRequest;
 import com.server.ggini.domain.locationAuth.dto.response.LocationAuthResponse;
 import com.server.ggini.global.annotation.AuthUser;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "회원", description = "회원 관련 API")
 @RestController
 @RequestMapping("/api/v1/locationAuth")
 @RequiredArgsConstructor
