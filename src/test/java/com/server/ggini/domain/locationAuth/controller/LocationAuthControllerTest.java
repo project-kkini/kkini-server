8d92f2fe7fa03c02d0d1fd5c6511c97bf26a6e56
박세준 <74056843+sejoon00@users.noreply.github.com>
2025-05-17 16:40:50 +0900

    feat: 검색

--- src/test/java/com/server/ggini/domain/locationAuth/controller/LocationAuthControllerTest.java
+++ src/test/java/com/server/ggini/domain/locationAuth/controller/LocationAuthControllerTest.java
@@ -4,18 +4,8 @@
 //import com.server.ggini.domain.locationAuth.dto.request.LocationAuthRequest;
 //import com.server.ggini.domain.locationAuth.dto.response.LocationAuthResponse;
 //import com.server.ggini.domain.locationAuth.service.LocationAuthService;
-//import com.server.ggini.domain.member.domain.CompanyLocation;
 //import com.server.ggini.domain.member.domain.Member;
-//import com.server.ggini.domain.member.domain.MemberRole;
-//import com.server.ggini.domain.member.domain.OauthInfo;
-//import com.server.ggini.domain.member.repository.MemberRepository;
-//import com.server.ggini.global.annotation.AuthenticationArgumentResolver;
 //import com.server.ggini.global.error.GlobalExceptionHandler;
-//import com.server.ggini.global.properties.jwt.JwtProperties;
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.security.Keys;
-//import java.security.Key;
-//import java.util.Date;
 //import org.junit.jupiter.api.BeforeEach;
 //import org.junit.jupiter.api.DisplayName;
 //import org.junit.jupiter.api.Test;
@@ -23,73 +13,38 @@
 //import org.mockito.InjectMocks;
 //import org.mockito.Mock;
 //import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.http.MediaType;
-//import org.springframework.test.context.ActiveProfiles;
 //import org.springframework.test.web.servlet.MockMvc;
 //import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.transaction.annotation.Transactional;
 //import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 //import static org.mockito.ArgumentMatchers.any;
 //import static org.mockito.BDDMockito.given;
-//import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 //import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import java.util.Optional;
-//import org.springframework.web.context.WebApplicationContext;
 //
-//@Transactional
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//@ActiveProfiles("test")
 //@ExtendWith(MockitoExtension.class)
 //class LocationAuthControllerTest {
 //
+//    private MockMvc mockMvc;
 //    private ObjectMapper objectMapper;
 //
 //    @Mock
 //    private LocationAuthService locationAuthService;
 //
-//    @Mock
-//    private MemberRepository memberRepository;
-//
 //    @InjectMocks
 //    private LocationAuthController locationAuthController;
-//
-//    @Autowired
-//    private JwtProperties jwtProperties;
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private WebApplicationContext context;
-//
-//    private String validToken;
-//    private Key key;
 //
 //    @BeforeEach
 //    void setUp() {
 //        objectMapper = new ObjectMapper();
 //
-//        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-//                .apply(springSecurity()).build();
-//
-//        key = Keys.hmacShaKeyFor(jwtProperties.accessTokenSecret().getBytes());
-//        Date issuedAt = new Date();
-//        Date expiredAt = new Date(issuedAt.getTime() + jwtProperties.accessTokenExpirationMilliTime());
-//        validToken = Jwts.builder()
-//                .setIssuer(jwtProperties.issuer())
-//                .setSubject("1")
-//                .claim("role", "ROLE_USER")
-//                .setIssuedAt(issuedAt)
-//                .setExpiration(expiredAt)
-//                .signWith(key)
-//                .compact();
+//        // MockMvc 설정 - @AuthUser 어노테이션을 처리할 수 있는 ArgumentResolver 추가
+//        mockMvc = MockMvcBuilders.standaloneSetup(locationAuthController)
+//                .setControllerAdvice(new GlobalExceptionHandler())
+//                .setValidator(new LocalValidatorFactoryBean())
+//                .build();
 //    }
 //
 //    @Test
@@ -101,14 +56,12 @@
 //        LocationAuthRequest request = new LocationAuthRequest(latitude, longitude);
 //        LocationAuthResponse response = new LocationAuthResponse(1L, "강남역");
 //
-//
 //        // when
 //        given(locationAuthService.authenticateLocation(any(Member.class), any(LocationAuthRequest.class)))
 //                .willReturn(response);
 //
 //        // then
 //        mockMvc.perform(post("/api/v1/locationAuth")
-//                        .header("Authorization", "Bearer " + validToken)
 //                        .contentType(MediaType.APPLICATION_JSON)
 //                        .content(objectMapper.writeValueAsString(request)))
 //                .andDo(print())
@@ -125,7 +78,6 @@
 //
 //        // then
 //        mockMvc.perform(post("/api/v1/locationAuth")
-//                        .header("Authorization", "Bearer " + validToken)
 //                        .contentType(MediaType.APPLICATION_JSON)
 //                        .content(invalidRequest))
 //                .andDo(print())
